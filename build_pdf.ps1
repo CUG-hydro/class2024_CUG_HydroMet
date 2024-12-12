727da70239d63b8ded7030f3a434f9c315a9adc1
Dongdong Kong <kongdd@users.noreply.github.com>
2024-12-12 17:15:29 +0800

    fix章节编号

--- build_pdf.ps1
+++ build_pdf.ps1
@@ -38,11 +38,11 @@ $options = "--pdf --pdf-outlines true --pdf-outlines.pages false" #
 # build_marp "./ch03_位势高度与气压场.md"
 # build_marp "./ch04_第1讲-水汽通量与暴雨分析.md"
 # build_marp "./ch04_第2讲-辐散与垂直运动.md"
-build_marp "./ch02_基础的气象变量.md"
-build_marp "./ch04_辐射与能量平衡.md"
+# build_marp "./ch02_基础的气象变量.md"
+# build_marp "./ch04_辐射与能量平衡.md"
 
 # build_marp "./ch05_太阳辐射.md"
-# build_marp "./ch06_大气环流.md"
+build_marp "./ch07_位势高度与气压场.md"
 # build_marp "./ch07_天气系统.md"
 
 # build_marp "./地信研究生_现代气候学_part1.md"
